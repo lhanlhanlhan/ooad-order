42aac54d73b846a576148fca17d22fb47adef699
lhanlhanlhan <han@han-li.cn>
2020-12-09 22:14:06 +0800

    9/12/2020 晚安

--- order-require/src/main/java/cn/edu/xmu/ooad/order/require/ICouponService.java
+++ order-require/src/main/java/cn/edu/xmu/ooad/order/require/ICouponService.java
@@ -1,4 +1,24 @@
 package cn.edu.xmu.ooad.order.require;
 
+import cn.edu.xmu.ooad.order.require.models.CouponActivityInfo;
+
 public interface ICouponService {
+
+    /**
+     * 根据 优惠券 id、买家 id 获取优惠券并返回所绑定的优惠活动
+     * @param couponId 优惠券 id
+     * @param customerId 买家 id
+     * @return 优惠活动资料，如果买家并不拥有此张优惠券，就返回 null
+     */
+    CouponActivityInfo getCoupon(Long customerId, Long couponId);
+
+
+    /**
+     * 根据 优惠活动 id 获取优惠活动
+     *
+     * @param couponActivityId 优惠活动 id
+     * @return 优惠活动资料
+     */
+    CouponActivityInfo getCouponActivity(Long couponActivityId);
+
 }
